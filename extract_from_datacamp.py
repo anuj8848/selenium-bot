aa34195dac9fdf674d3c99cccd39327fd945be3d
anuj8848 <anujgiri8848@gmail.com>
2024-04-25 23:08:13 +0545

    counter function implemented

--- extract_from_datacamp.py
+++ extract_from_datacamp.py
@@ -5,19 +5,23 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
 
+def word_counter(word_list):
+    count=0
+    for word in word_list:
+        if word=='data' or word=='Data' or word=='datas' or word=='Datas':
+            count+=1
+    return count
+
+
 driver=webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.maximize_window()
 
 driver.get("https://www.datacamp.com/tutorial/pandas")
 
 link=driver.find_element(By.XPATH,'//*[@id="main-container"]/div[3]/aside[1]/div/div/nav/div/ul[1]/li/a').click()
-sleep(3)
 content_page=driver.find_element(By.XPATH,'//*[@id="main-container"]/div[3]').text
 word_list=content_page.split(" ")
-count=0
-for i in word_list:
-    if i=='data' or i=='Data' or i=='datas' or i=='Datas':
-        count+=1
-print(count)
-sleep(3)
+
+
+print(word_counter(word_list))
 driver.quit()
