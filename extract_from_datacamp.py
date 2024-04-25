fb3cf377bf31214f19eaaba1dee6c85b9e0859e6
anuj8848 <anujgiri8848@gmail.com>
2024-04-25 23:30:00 +0545

    improved and more efficient using regular expression

--- extract_from_datacamp.py
+++ extract_from_datacamp.py
@@ -4,29 +4,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
+import re
 
-website=input("enter website to automate\n")
-word2count=input('enter required word to count: ')
-
-
-
-def word_counter(word,word_list):
-    count=0
-    for wrd in word_list:
-        if wrd==word or wrd==word or wrd==word or wrd==word:
-            count+=1
-    return count
 
 
 driver=webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.maximize_window()
 
-driver.get(website)
+driver.get("https://www.datacamp.com/tutorial/pandas")
 
-link=driver.find_element(By.XPATH,'/html/body/div[4]').text
-# content_page=driver.find_elements(By.XPATH,'//*[@id="main-container"]/div[3]').text
-word_list=link.split(" ")
+link=driver.find_element(By.XPATH,'/html').text
+# content_page=driver.find_element(By.XPATH,'//*[@id="main-container"]/div[3]').text
+
+matches = re.findall(r'\b\w*Data\w*\b', link, re.IGNORECASE)
+print(len(matches))
 
 
-print(word_counter(word2count,word_list))
 driver.quit()
