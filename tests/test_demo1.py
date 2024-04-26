68667d15116d0c16ef324cdbfa72755ac718a8ae
anuj8848 <anujgiri8848@gmail.com>
2024-04-26 23:56:50 +0545

    test class added

--- tests/test_demo1.py
+++ tests/test_demo1.py
@@ -1,13 +1,13 @@
-
-
 def func(x):
     return x+1
 
-def test_1():
-    assert func(2) ==3
-    
-def test_2():
-    assert func(6) ==7
-    
-def test_3():
-    assert func(4) ==5
+
+class TestCases:
+    def test_1(self):
+        assert func(2) ==3
+        
+    def test_2(self):
+        assert func(6) ==9
+        
+    def test_3(self):
+        assert func(4) ==5
